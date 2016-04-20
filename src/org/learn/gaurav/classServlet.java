5b00ec1e092b3780785a751a1f97673dedf9b5ca
Gaurav Rajkumar <gaurav8k@gmail.com>
2016-04-20 17:50:57 +0530

    Working

--- src/org/learn/gaurav/classServlet.java
+++ src/org/learn/gaurav/classServlet.java
@@ -3,18 +3,27 @@ package org.learn.gaurav;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class classServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			//System.out.println("Testing from Class Servlet");
 		response.setContentType("text/html");
-		String name = request.getParameter("name");
 		PrintWriter writer = response.getWriter();
-		writer.println("Am from classServlet(GET Method) " + name);	
+		String name = request.getParameter("name");
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+		if(name != null){
+			session.setAttribute("FirstName", name);
+			context.setAttribute("FirstName", name);
+		}		
+		writer.println("Am from classServlet(GET Method) " + name + " The session is :" + (String)session.getAttribute("FirstName"));
+		writer.println("The context is: " + (String)context.getAttribute("FirstName"));
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//System.out.println("Testing from Class Servlet");
